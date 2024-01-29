9f02270d325cc94478e1ba69dd5a3d2e82bc1e0e
miivanov2019 <145435880+miivanov2019@users.noreply.github.com>
2024-01-29 13:52:14 +0200

    FirstVersion

---
+++ task123/Models/Lesson.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace task123.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
